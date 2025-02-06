8288d096303e39c029ec9753522cb30327462b59
MuhammadKamran26 <kamran.hanfi@gmail.com>
2025-02-06 11:46:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+let exp = prompt("Enter your exp");
+let js = prompt("Do you know JS?");
+let city = prompt("Enter your city");
+let eSalary = prompt("Enter your expected salary");
+
+if (exp >= 2 && js == "yes" && city == "karachi" && eSalary <= 100000) {
+    console.log("You're Hired!");
+} 
+    let reason = 'reason'
+    else{
+if (exp < 2) reason += 'no experince'
+if (js.toLowerCase()) reaso!== 'no experince'
+if (city.toLowerCase()) reason!== 'no experince'
+if (eSalary>=100000) reason+= 'high demand'
+
+}
+  
+    console.log("You're Fired!");
+
+
+// let reason = 'reason'
+// if (exp < 2) reason += 'no experince'
+// if (js.toLowerCase()) reason !== 'no experince'
+// if (city.toLowerCase()) reason !== 'no experince'
+// if (eSalary>=100000) reason += 'high demand'
+
